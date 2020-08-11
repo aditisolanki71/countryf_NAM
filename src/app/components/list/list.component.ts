98bdba909d2393f5350d6d54fe028337ab4c13a9
Aditi <adtsolanki22@gmail.com>
2020-08-11 12:17:06 +0530

    Country List Page

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CountryService } from '../../shared/country.service'
+import { Country } from '../../country';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  public countries:Country[];
+  constructor(private _countryService:CountryService) { }
 
   ngOnInit(): void {
-  }
+    this.readCountries();
+  } 
 
+  readCountries() {
+    this._countryService.readCountries().subscribe(
+      data => {
+        console.log('data',data)
+        this.countries = data['msg'];
+      },
+      error => {
+        console.log('err',error)
+      }
+    )
+  }
 }
